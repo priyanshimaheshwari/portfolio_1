33941153e0e6fa83eba8358aa1e5ac8e500f708f
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-04-07 15:30:20 +0530

    completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import {FiInstagram} from 'react-icons/fi'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-blue-400 text-center'>
+        <a href="#" className=' font-bold text-black text-4xl pt-14'>PRIYANSHI</a>
+
+        <ul className='flex text-black items-center justify-center gap-4 py-4'>
+        <li><a href='#' className='hover:underline'>Home</a></li>
+        <li><a href='#About' className='hover:underline'>About</a></li>
+        <li><a href='#Skills' className='hover:underline'>Skills</a></li>
+        <li><a href='#Portfolio' className='hover:underline'>Projects</a></li>
+        <li><a href='#Contact' className='hover:underline'>Contact</a></li>
+        </ul>
+
+        <div className='items-center justify-center flex gap-6 py-12'>
+            <a href="https://www.instagram.com/priyanshimaheshwari_/"><FiInstagram color='black' size={30}/></a>
+            <a href="https://www.linkedin.com/in/priyanshi-maheshwari-438bab225?original_referer="><BsLinkedin color='black' size={30}/></a>
+            <a href="https://github.com/priyanshimaheshwari"><FaGithub color='black' size={30}/></a>
+        </div>
+
+        <div className='py-4 text-gray-500'>&copy; Priyanshi.All rights reserved.</div>
+    </div>
   )
 }
 
